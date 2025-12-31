e5b9de4ada082e70c734df15b0c3d031ea38949e
Vepoe <58576156+Vepoe@users.noreply.github.com>
2025-12-31 10:48:04 +0800

    Update webrtc.js

--- webrtc.js
+++ webrtc.js
@@ -1,1 +1,9 @@
-Object.defineProperty(navigator, 'rtcPeerConnection', {value: undefined});
+// 禁用标准的 WebRTC 接口
+Object.defineProperty(navigator, 'RTCPeerConnection', { value: undefined });
+Object.defineProperty(navigator, 'webkitRTCPeerConnection', { value: undefined });
+Object.defineProperty(navigator, 'mozRTCPeerConnection', { value: undefined });
+// 移除 mediaDevices 探测
+if (navigator.mediaDevices) {
+    Object.defineProperty(navigator.mediaDevices, 'enumerateDevices', { value: undefined });
+}
+$done({});
